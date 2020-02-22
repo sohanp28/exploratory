9d5d608d0b8c692018ef691337549bf263ed4630
sohanp28 <pradhansohan965@gmail.com>
2020-02-22 21:04:57 +0530

    plot4

---
+++ plot4.R
@@ -0,0 +1,23 @@
+setwd("C:/Users/MAHE/expo")
+data<- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+                      nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+d1 <- subset(data, Date %in% c("1/2/2007","2/2/2007"))
+d1$Date <- as.Date(d1$Date, format="%d/%m/%Y")
+datetime <- paste(as.Date(d1$Date), d1$Time)
+d1$Datetime <- as.POSIXct(datetime)
+
+par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
+with(d1, {
+  plot(Global_active_power~Datetime, type="l", 
+       ylab="Global Active Power", xlab="")
+  plot(Voltage~Datetime, type="l", 
+       ylab="Voltage", xlab="")
+  plot(Sub_metering_1~Datetime, type="l", 
+       ylab="Global Active Power", xlab="")
+  lines(Sub_metering_2~Datetime,col='Red')
+  lines(Sub_metering_3~Datetime,col='Blue')
+  legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
+         legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+  plot(Global_reactive_power~Datetime, type="l", 
+       ylab="Global Rective Power",xlab="")
+})
